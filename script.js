b486e3abc5bbf2c6294017fe348363c29a44cf29
MelamSrihari7 <melamsrihari@gmail.com>
2025-11-15 19:03:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const priceList=Document.querySelectorAll(.prices)
+	let total=0;
+	priceList.forEach((price)=>total+=Number(price.textContent));
+	const table = document.querySelector("table");
+  const row = document.createElement("tr");
+  const cell = document.createElement("td");
+      cell.id = "ans";
+  cell.textContent = total;
+
+  // Make it full-width (if table has 2 cols)
+  cell.setAttribute("colspan", "2");
+	 row.appendChild(cell);
+  table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
